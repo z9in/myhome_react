2c8098291b99bbd30a77e84173c52caab13bb689
z9in <113665653+z9in@users.noreply.github.com>
2023-02-17 09:38:06 +0900

    Update works.js

--- src/pages/works.js
+++ src/pages/works.js
@@ -19,25 +19,22 @@ export default function Works() {
             <h3>주요 구현 내용</h3>
             <ul>
               <li>OpenWeatherMap API 활용</li>
-              <li>클릭 시 우측 그리드에 해당 지역 날씨 상세정보 확인 반영</li>
-              <li>지도 내 지역 hover 시 이미지상자를 띄워 날씨 이미지 반영</li>
-              <li>지역 구분은 지도 위에 박스그리드 씌어 구분</li>
+              <li>접속 시 상단 지도를 통해 현재 위치 표시</li>
+              <li>하단 정보 창에 현재 온도와 날씨 정보 표시</li>
+              <li>지도 내 특정 위치 클릭(탭) 시 해당 지역의 날씨 확인</li>
             </ul>
             <h3>개선 가능성</h3>
             <ul>
-              <li>
-                지도 위 그리드 생성 시 HTML 페이지 내에 작성하였으나
-                자바스크립트 createEliment 등을 생성한다면 더 편리하고 상세하게
-                작성 가능
-              </li>
-              <li>지도 API를 활용하여 위도 경도 확인 등을 진행할 수 있음</li>
+              <li>expo를 사용하지 않고 빌드 및 배포할 수 있도록 개발</li>
+              <li>native의 기능들을 활용할 수 있도록 방향 설계 필요</li>
+              <li>지도 클릭 외에 지역 선택 혹은 검색할 수 있는 기능 반영</li>
             </ul>
             <div className="works_btn">
-              <a href="https://github.com/z9in/weatherApp1" target="_blank">
+              <a href="https://github.com/z9in/weather_app_react_native" target="_blank">
                 소스코드
               </a>
               <a href="https://z9in.github.io/weatherApp1/" target="_blank">
-                구현주소
+                다운로드
               </a>
             </div>
           </div>
@@ -68,7 +65,7 @@ export default function Works() {
               >
                 소스코드
               </a>
-              <a href="https://z9in.github.io/puzzle/" target="_blank">
+              <a href="https://web-toiletsearch-react-422t024lbonwoan.gksl2.cloudtype.app/" target="_blank">
                 구현주소
               </a>
             </div>
